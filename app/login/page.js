2d316f28d6a9dd0c5fd4d8a04eab4c76917e6fb3
satis-fintech <devsatis2024@gmail.com>
2024-05-12 00:14:00 +0100

    login

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Login = () => {
-  return <div>This sis a login page</div>;
+  return (
+    <div className="container mx-auto flex items-center justify-center bg-slate-400">
+      <h1>Login</h1>
+      <form className="flex flex-col gap-4 w-[340px] ">
+        <input type="text" />
+        <input type="password" />
+        <button className="btn">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;

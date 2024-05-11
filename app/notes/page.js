6647dd12f65c9f0472482d2919c75c537f37dfd4
satis-fintech <devsatis2024@gmail.com>
2024-05-11 20:36:41 +0100

    ajout page note2

--- app/notes/page.js
+++ app/notes/page.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Note = () => {
-  return <div>notes on this page</div>;
+  return (
+    <div>
+      notes on this page2
+      <h1>testde page</h1>
+    </div>
+  );
 };
 
 export default Note;
